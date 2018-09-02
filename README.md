e2167316f8e5d8d20c3d01a4d5ae3951f9f840be
Nigel Scott <nige@zaonce.com>
2018-09-02 21:55:20 +0100

    Version 0.0.1

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 This a very simple extension that covers the whole text editor in colour when there are errors or warnings present.
 
-*Note: This works by overriding your theme settings.*
+*Note: This works by overriding your theme settings. Your original settings are backed up in* `awooga.originalColorCustomizations`.
 
 By default it is enabled for all file extensions (except files without extensions).
 
-When the current file has an extension and an available formatter, a button is displayed in the status bar (a megaphone with a tick or a cross next to it) indicating if **Awooga!** is enabled for the current file extension:
+When the current file has an extension, a button is displayed in the status bar (a megaphone with a tick or a cross next to it) indicating if **Awooga!** is enabled for the current file extension:
 
 <img src="https://raw.githubusercontent.com/Gruntfuggly/awooga/master/button.png">
 
@@ -14,30 +14,33 @@ Click the button to disable or enable.
 
 Once a file extension has been enabled or disabled, the state is stored in your settings and the extension will no longer be enabled for all file extensions.
 
-### TODO
-- [ ] Store original settings on first activation
-- [ ] Handle problem matchers
 
 ## Configuration
 
 Use `awooga.errorColour` and `awooga.warningColour` to change the colours of the overlays.
 
-`awooha.enabled` can be manually modified to set the state for specific file extensions.
+`awooga.enabled` can be manually modified to set the state for specific file extensions.
 
 The position of the status bar button can be configured using `awooga.buttonAlignment` and `awooga.buttonPriority`.
+
 
 ## Installing
 
 You can install the latest version of the extension via the Visual Studio Marketplace [here](https://marketplace.visualstudio.com/items?itemName=Gruntfuggly.awooga).
 
+
 ### Source Code
 
 The source code is available on GitHub [here](https://github.com/Gruntfuggly/awooga).
 
+
 ## Known Issues
 
 - Files without extensions are not considered.
-- Only works when diagnostics are available (not problem matchers). 
+- Only works when diagnostics are available (not problem matchers - yet).
+
 
 ## Credits
+
+Icon made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
 
--- extension.js
+++ extension.js
@@ -1,17 +1,94 @@
 var vscode = require( 'vscode' );
+var path = require( 'path' );
+
+var button;
 
 var location = vscode.workspace.getConfiguration( 'awooga' ).get( 'location' );
 
 function setBackgroundColour( colour )
 {
     var colours = vscode.workspace.getConfiguration( 'workbench' ).get( 'colorCustomizations' );
+    vscode.workspace.getConfiguration( 'awooga' ).update( 'originalColorCustomizations', colours, false );
     colours[ location ] = colour;
     vscode.workspace.getConfiguration( 'workbench' ).update( 'colorCustomizations', colours, false );
 }
 
+function resetColour()
+{
+    location = vscode.workspace.getConfiguration( 'awooga' ).get( 'location' );
+    setBackgroundColour( undefined );
+}
+
 function activate( context )
 {
+    function backupOriginalColours()
+    {
+        var originalColours = vscode.workspace.getConfiguration( 'awooga' ).get( 'originalColorCustomizations' );
+        if( originalColours === {} )
+        {
+            var colours = vscode.workspace.getConfiguration( 'workbench' ).get( 'colorCustomizations' );
+            vscode.workspace.getConfiguration( 'awooga' ).update( 'originalColorCustomizations', colours, false );
+        }
+    }
+
     var lastLevel;
+
+    function getExtension()
+    {
+        var editor = vscode.window.activeTextEditor;
+        if( editor && editor.document )
+        {
+            ext = path.extname( editor.document.fileName );
+            if( ext && ext.length > 1 )
+            {
+                return ext.substr( 1 );
+            }
+        }
+        return "";
+    }
+
+    function isEnabled()
+    {
+        var extension = getExtension();
+        var enabled = vscode.workspace.getConfiguration( 'awooga' ).get( 'enabled' );
+        return Object.keys( enabled ).length === 0 || ( extension.length > 0 && enabled[ extension ] );
+    }
+
+    function updateButton()
+    {
+        var extension = getExtension();
+
+        var enabled = isEnabled() === true;
+
+        button.text = "$(megaphone) $(" + ( enabled ? "check" : "x" ) + ")";
+        button.command = 'awooga.' + ( enabled ? 'disable' : 'enable' );
+        button.tooltip = ( enabled ? 'Disable' : 'Enable' ) + " Awooga! for ." + extension + " files";
+
+        if( extension.length > 0 )
+        {
+            button.show();
+        }
+        else
+        {
+            button.hide();
+        }
+    }
+
+    function createButton()
+    {
+        if( button )
+        {
+            button.dispose();
+        }
+
+        button = vscode.window.createStatusBarItem(
+            vscode.workspace.getConfiguration( 'awooga' ).get( 'buttonAlignment' ) + 1,
+            vscode.workspace.getConfiguration( 'awooga' ).get( 'buttonPriority' ) );
+
+        context.subscriptions.push( button );
+
+        updateButton();
+    }
 
     function refresh()
     {
@@ -48,30 +125,73 @@ function activate( context )
 
             if( level !== lastLevel )
             {
-                setBackgroundColour( colour );
+                if( colour && isEnabled() )
+                {
+                    setBackgroundColour( colour );
+                }
+                else
+                {
+                    resetColour();
+                }
                 lastLevel = level;
             }
 
         }
     }
 
-    context.subscriptions.push( vscode.languages.onDidChangeDiagnostics( refresh ) );
-    context.subscriptions.push( vscode.window.onDidChangeActiveTextEditor( refresh ) );
-    context.subscriptions.push( vscode.workspace.onDidChangeConfiguration( function( e )
+    function configure( shouldEnable )
     {
-        if( e.affectsConfiguration( 'awooga' ) )
+        var enabled = vscode.workspace.getConfiguration( 'awooga' ).get( 'enabled' );
+        var extension = getExtension();
+        enabled[ extension ] = shouldEnable;
+        vscode.workspace.getConfiguration( 'awooga' ).update( 'enabled', enabled, true );
+    }
+
+    context.subscriptions.push( vscode.workspace.onDidOpenTextDocument( function()
+    {
+        if( !button )
         {
-            lastLevel = undefined;
-            setBackgroundColour( undefined );
-            location = vscode.workspace.getConfiguration( 'awooga' ).get( 'location' );
-            refresh();
+            createButton();
+        }
+        else
+        {
+            updateButton();
         }
     } ) );
+
+    context.subscriptions.push( vscode.languages.onDidChangeDiagnostics( refresh ) );
+    context.subscriptions.push( vscode.window.onDidChangeActiveTextEditor( function( e )
+    {
+        updateButton();
+        refresh();
+    } ) );
+
+    context.subscriptions.push( vscode.workspace.onDidChangeConfiguration( function( e )
+    {
+        if( e.affectsConfiguration( 'awooga.enabled' ) )
+        {
+            lastLevel = undefined;
+            resetColour();
+            refresh();
+            updateButton();
+        }
+        else if(
+            e.affectsConfiguration( 'awooga.buttonAlignment' ) ||
+            e.affectsConfiguration( 'awooga.buttonPriority' ) )
+        {
+            createButton();
+        }
+    } ) );
+
+    context.subscriptions.push( vscode.commands.registerCommand( 'awooga.enable', function() { configure( true ); } ) );
+    context.subscriptions.push( vscode.commands.registerCommand( 'awooga.disable', function() { configure( false ); } ) );
+
+    backupOriginalColours();
 }
 
 function deactivate()
 {
-    setBackgroundColour( undefined );
+    resetColour();
 }
 
 exports.activate = activate;
